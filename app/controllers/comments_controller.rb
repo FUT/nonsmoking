99d0a50c390456ecced5cd8584526086e017f7bf
yury.kaliada <fut.wrk@gmail.com>
2013-05-24 15:16:54 +0300

    Kaminari pagination to comments
    Signed-off-by: yury.kaliada <fut.wrk@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   load_resource :diary
 
   def create
-    Comment.build_from(@diary, current_user.id, params[:comment][:body]).save
+    Comment.build_from(@diary, current_user.id, params[:new_comment][:body]).save
 
     redirect_to @diary
   end
